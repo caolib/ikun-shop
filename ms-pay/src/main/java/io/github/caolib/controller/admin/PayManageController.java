17617772186fa67860099580b10d735788ea0ba1
孤独的Lonely <1265501579@qq.com>
2025-04-10 11:19:17 +0800

    ✨ feat(ms-pay): 添加历史数据接口

--- ms-pay/src/main/java/io/github/caolib/controller/admin/PayManageController.java
+++ ms-pay/src/main/java/io/github/caolib/controller/admin/PayManageController.java
@@ -1,16 +1,23 @@
 package io.github.caolib.controller.admin;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.github.caolib.domain.R;
 import io.github.caolib.domain.vo.PayDetailResVO;
 import io.github.caolib.domain.vo.PayStatisticVO;
 import io.github.caolib.service.IPayOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,4 +50,29 @@ public class PayManageController {
         return payOrderService.payDetail(start, end);
     }
 
+    @GetMapping("/history")
+    public R<List<PayDetailResVO>> payHistory() {
+        try {
+            // 从资源文件夹中读取 history.json
+            Resource resource = new ClassPathResource("history.json");
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+            // 解析 JSON 文件
+            JsonNode jsonNode = objectMapper.readTree(resource.getInputStream());
+            JsonNode dataNode = jsonNode.get("data");
+
+            List<PayDetailResVO> result = new ArrayList<>();
+            if (dataNode.isArray()) {
+                for (JsonNode node : dataNode) {
+                    PayDetailResVO resVO = objectMapper.treeToValue(node, PayDetailResVO.class);
+                    result.add(resVO);
+                }
+            }
+            return R.ok(result);
+        } catch (Exception e) {
+            return R.error("获取历史数据失败");
+        }
+    }
 }
